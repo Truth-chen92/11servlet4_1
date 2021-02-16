7b13b60337ee33c45ee9ccde7f4e7465dcd956b5
Truth <527768695@qq.com>
2021-02-16 20:18:19 +0800

    11servlet4_1

--- src/main/java/cn/tedu/controller/UploadServlet.java
+++ src/main/java/cn/tedu/controller/UploadServlet.java
@@ -6,12 +6,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 @MultipartConfig
 @WebServlet(name = "UploadServlet",urlPatterns = "/upload")
 public class UploadServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("utf-8");
+        String title=request.getParameter("title");
+        Part part=request.getPart("pic");
+        String info=part.getHeader("content-disposition");
+        String suffix=info.substring(info.lastIndexOf("."),info.length()-1);
+        System.out.println("title:"+title);
+        System.out.println("后缀名:"+suffix);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
